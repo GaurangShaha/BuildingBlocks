07a36b016ee0938449da96fdba7e61c498ffe068
Gaurang <gaurang.shaha@gmail.com>
2025-11-13 12:27:09 +0530

    Refactored encrypted file handling

--- storage/src/main/java/building/blocks/storage/encrypted/source/local/EncryptedFileLocalSource.kt
+++ storage/src/main/java/building/blocks/storage/encrypted/source/local/EncryptedFileLocalSource.kt
@@ -27,8 +27,8 @@ internal class EncryptedFileLocalSource(
                 checkPublicFileEncryptionAllowed(location)
 
                 cryptoManager.encrypt(inputStream).use { encryptedStream ->
-                    localFileSource.save(fileName, location, encryptedStream)
-                }.getOrThrow()
+                    localFileSource.save(fileName, location, encryptedStream).getOrThrow()
+                }
             }
         }
     }
@@ -41,9 +41,8 @@ internal class EncryptedFileLocalSource(
             executeSafely {
                 checkPublicFileEncryptionAllowed(location)
 
-                localFileSource.read(fileName, location).map { encryptedInputStream ->
-                    cryptoManager.decrypt(encryptedInputStream)
-                }.getOrThrow()
+                val encryptedInputStream = localFileSource.read(fileName, location).getOrThrow()
+                cryptoManager.decrypt(encryptedInputStream)
             }
         }
     }
